5d9887e125c4b14b2095f800db36c53f06cb7a56
edwinisnow <edwin04@gmail.com>
2019-07-08 00:25:40 +0100

    Query records from db

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
+// Constructor
 var Sequelize = require('sequelize');
 
+// Connection
 var connection = new Sequelize('demo_schema', 'root', '', {
   host: 'localhost',
-  dialect: 'mariadb'
+  dialect: 'mariadb',
+  dialectOptions: { connectTimeout: 1000 }
 });
 
-var Article = connection.define('atricle', {
+// Model Definition
+var Article = connection.define('article', {
   title: Sequelize.STRING,
   body: Sequelize.TEXT
 });
 
-connection.sync();
+// Sync
+connection.sync().then(function() {
+  // Create a record
+  // Article.create({
+  //   title: 'demo-title',
+  //   body: 'demo-body'
+  // });
+
+  // Query a record by Primary Key
+  // Article.findByPk(1).then(function(article) {
+  //   console.log('Log: -----> : article.dataValues', article.dataValues);
+  // });
+
+  // Query all records
+  Article.findAll().then(function(articles) {
+    console.log('Log: -----> : articles.length', articles.length);
+  });
+});
